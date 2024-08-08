d1d451dbe836c1406103b0ddc0887205ec5684d7
ebraimsambo <ebraimsambo@gmail.com>
2024-08-08 20:39:59 +0100

    footer

---
+++ src/components/bases/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+  return (
+    <footer className='flex items-center justify-center p-8 max-w-96 m-auto'>
+        <p className='text-center text-xs'>Codificado e mantido pela Web.AO &copy; {currentYear} </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,15 +1,17 @@
 import React, { FC, ReactNode } from 'react'
 import NavBar from '../bases/nav-bar'
+import Footer from '../bases/footer'
 
-interface App{
-    children: ReactNode
+interface App {
+  children: ReactNode
 }
-const AppLayout: FC<App> = ({children}) => {
+const AppLayout: FC<App> = ({ children }) => {
   return (
-    <div> 
-        <NavBar />
-        <main>{children}</main>
-     </div>
+    <div>
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </div>
   )
 }
 
